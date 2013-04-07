32e83454ba684509ed2e88fd450d666d9c7940f8
leeview <liviu_gb@yahoo.com>
2013-04-07 02:15:11 +0200

    unix sockets scgi + fcgi test server apps 	new file:   benchmarks/ufcgi1/main.go 	new file:   benchmarks/uscgi1/main.go

---
+++ benchmarks/ufcgi1/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	"runtime"
+)
+
+func main() {
+	log.Println("listening for nginx connections at localhost:8081")
+	log.Println("press ctrl + c to close...")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	addr, err := net.ResolveUnixAddr("unix", "/tmp/gofcgi.socket")
+	if err != nil {
+		return
+	}
+	listener, err := net.ListenUnix("unix", addr)
+	if err != nil {
+		return
+	}
+	defer listener.Close()
+
+	http.HandleFunc("/", index)
+
+	err = fcgi.Serve(listener, nil)
+	if err != nil {
+		log.Println("fcgi.Serve:", err.Error())
+	}
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
+	fmt.Fprintf(w, header)
+	fmt.Fprintf(w, "Hello world, "+req.URL.Path)
+	fmt.Fprintf(w, footer)
+}
+
+var header = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title> GO + FCGI </title>
+	<style>	.centered {margin-left:auto;margin-right:auto;width:90%;} </style>
+</head>
+<body>
+	<h3 class="centered"> GO + FCGI </h3>
+	<div class="centered">`
+
+var footer = `
+	</div>
+</body>
+</html>`
---
+++ benchmarks/uscgi1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	//scgi "github.com/leeview/goscgi"
+	scgi "goscgi"
+	"log"
+	"runtime"
+)
+
+func main() {
+	log.Println("listening for nginx connections at localhost:8080")
+	log.Println("press ctrl + c to close...")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	serv := scgi.NewServer(scgi.NewSettings())
+	serv.AddHandler("/", index)
+
+	//err := serv.ListenTcp(":8080")
+	err := serv.ListenUnix("/tmp/goscgi.socket")
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func index(req *scgi.Request) *scgi.Response {
+	var body bytes.Buffer
+	body.WriteString(header)
+	body.WriteString("Hello world, " + req.URL.Path)
+	body.WriteString(footer)
+	return scgi.NewResponse(scgi.RespCodeOK, scgi.RespTypeHtml, body.Bytes())
+}
+
+var header = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title> GO + SCGI </title>
+	<style>	.centered {margin-left:auto;margin-right:auto;width:90%;} </style>
+</head>
+<body>
+	<h3 class="centered"> GO + SCGI </h3>
+	<div class="centered">`
+
+var footer = `
+	</div>
+</body>
+</html>`
